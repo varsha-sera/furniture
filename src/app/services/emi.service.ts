7671c6e390b02f47646516197f587b0f4f4b2ba5
varsha-sera <vsera0927@gmail.com>
2022-04-23 12:24:55 +0530

    changes

---
+++ src/app/services/emi.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmiService {
+
+  constructor(private http:HttpClient) { }
+
+  addEmi(intrest:any,downpayment:any,totalPrice:any){
+    let userId=localStorage.getItem('id');
+    return this.http.post("https://furniture-v1-app.herokuapp.com/api/emi/emientry",{emiTotal:totalPrice,emiIntrest:intrest,emiDownPayment:downpayment,products:"62629035d3089a54b136bc60",userId:userId});
+  }
+
+  placeOrder(mobile:any,address:any,email:any,quantity: any,amount:any,emi:any,productid:any){
+    let userId=localStorage.getItem('id');
+    return this.http.post("https://furniture-v1-app.herokuapp.com/api/order/order",{userId:userId,mobileNo:mobile,shippingAddress:address,orderQuantity:quantity,totalAmount:amount,orderPayment:emi,productId:productid});
+  }
+}
